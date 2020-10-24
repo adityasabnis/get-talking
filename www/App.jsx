c0f5297bd675c69365f843ec4eaff6bfab8328c0
Aarav Sabnis <sabnisaarav@gmail.com>
2020-10-24 17:24:53 +1100

    feat: working example and refactor custom input

--- www/App.jsx
+++ www/App.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import _ from "lodash";
+import CustomInput from "../src/mock/CustomInput";
+import { withSpeech } from "../src";
 
-class App extends React.Component {
-  render() {
-    return <div>Example here</div>;
-  }
-}
+const CustomInputWithSpeech = withSpeech(CustomInput, {
+  voiceName: "Alex",
+  speechTextPropName: "value",
+});
+
+const App = () => {
+  const [alertInputText, setAlertInputText] = useState("My name is John Snow");
+
+  return (
+    <div>
+      <div>Click inside the component</div> <br />
+      <div>Custom Input</div>
+      <CustomInputWithSpeech
+        value={alertInputText}
+        onValueChange={({ target }) =>
+          setAlertInputText(_.get(target, "value"))
+        }
+      />
+    </div>
+  );
+};
 
 export default App;
